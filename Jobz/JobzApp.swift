f742fcd4faaf7260b81ed83683694e7c24ccbb22
Tyler Yang <59151359+88Mangos@users.noreply.github.com>
2026-08-11 18:30:04 -0500

    Initial Commit

---
+++ Jobz/JobzApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobzApp.swift
+//  Jobz
+//
+//  Created by Tyler Yang on 8/11/26.
+//
+
+import SwiftUI
+
+@main
+struct JobzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JobzTests/JobzTests.swift
@@ -0,0 +1,17 @@
+//
+//  JobzTests.swift
+//  JobzTests
+//
+//  Created by Tyler Yang on 8/11/26.
+//
+
+import Testing
+@testable import Jobz
+
+struct JobzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
